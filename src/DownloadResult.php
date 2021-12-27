1e7d17851e5fa6848a327488691245d1930c0cc2
martyd420 <github-messages@pcdr.cz>
2021-12-27 21:12:25 +0100

    testuju... ale uz jdu spat, dodelam zitra :) :P

--- src/DownloadResult.php
+++ src/DownloadResult.php
@@ -7,21 +7,25 @@ class DownloadResult
     private string $headers;
     private string $body;
 
+
     public function __construct(string $headers, string $body)
     {
         $this->headers = $headers;
         $this->body = $body;
     }
 
+
     public function getHeaders(): string
     {
         return $this->headers;
     }
 
+
     public function getBody(): string
     {
         return $this->body;
     }
+
 
     public function __toString(): string
     {
--- src/SimpleCachedDownloader.php
+++ src/SimpleCachedDownloader.php
@@ -11,17 +11,15 @@ class SimpleCachedDownloader
     private string $cache_dir;
     private $curl;
 
+    private string $user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36';
 
-    /**
-     * SimpleCachedDownloader constructor.
-     * @param string $cache_dir
-     */
+
     public function __construct(string $cache_dir)
     {
         $this->cache_dir = realpath($cache_dir) . DIRECTORY_SEPARATOR;
 
         $this->curl = curl_init();
-        curl_setopt($this->curl, CURLOPT_USERAGENT,         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36');
+        curl_setopt($this->curl, CURLOPT_USERAGENT,         $this->user_agent);
         curl_setopt($this->curl, CURLOPT_RETURNTRANSFER,    true);
         curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER,    false);
         curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST,    false);
@@ -40,24 +38,10 @@ class SimpleCachedDownloader
      * @param int $max_age
      * @param int $sleep
      * @return DownloadResult
+     * @throws CacheNotWriteableException
      * @throws ConnectionErrorException
      */
     public function download(string $url, int $max_age = 86400, int $sleep = 0): DownloadResult
-    {
-        // separate functions for back compatibility
-        return $this->curl_get($url, $max_age, $sleep);
-    }
-
-
-    /**
-     * @param $url
-     * @param $max_age
-     * @param $sleep
-     * @return DownloadResult
-     * @throws ConnectionErrorException
-     * @throws CacheNotWriteableException
-     */
-    private function curl_get($url, $max_age, $sleep): DownloadResult
     {
         if (!is_dir($this->cache_dir) || !is_writeable($this->cache_dir)) {
             throw new CacheNotWriteableException('Permission denied in ' . $this->cache_dir);
@@ -87,6 +71,12 @@ class SimpleCachedDownloader
         }
 
         return new DownloadResult($header, $data);
+    }
+
+
+    public function setUserAgent(string $user_agent): void
+    {
+        $this->user_agent = $user_agent;
     }
 
 
